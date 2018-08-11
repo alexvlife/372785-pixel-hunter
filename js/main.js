38bb42b6a1b2d6c57c21d26a5c5cb37963a662de
alexvlife <alexvlife@gmail.com>
2018-08-11 12:56:55 +0300

    m1. 3-4. создана функция, которая по переданному номеру будет показывать соответствующий экран

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
+const gameField = document.querySelector(`#main`);
+
+const showScreen = (el) => {
+  gameField.innerHTML = ``;
+  gameField.appendChild(el.cloneNode(true));
+}
+
 const screens = Array.from(document.querySelectorAll(`template`))
                      .map((item) => item.content);
+
+let currentScreen = 0;
+
+const selectScreen = (index) => {
+  index = index < 0 ? screens.length - 1 : index;
+  index = index >= screens.length ? 0 : index;
+  currentScreen = index;
+  showScreen(screens[currentScreen]);
+}
+
+selectScreen(1);
