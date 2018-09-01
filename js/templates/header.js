a90adfdcb8a54113c22891771bd3d2133820b1b2
alexvlife <alexvlife@gmail.com>
2018-09-01 23:42:29 +0300

    m4. cr - fixed

--- js/templates/header.js
+++ js/templates/header.js
@@ -1,20 +1,17 @@
 const getHeaderTemplate = (stateTemplate) => {
-  let state = stateTemplate;
-  if (!stateTemplate) {
-    state = ``;
-  }
+  stateTemplate = (stateTemplate) ? stateTemplate : ``;
   return `<header class="header">
-  <button class="back">
-    <span class="visually-hidden">Вернуться к началу</span>
-    <svg class="icon" width="45" height="45" viewBox="0 0 45 45" fill="#000000">
-      <use xlink:href="img/sprite.svg#arrow-left"></use>
-    </svg>
-    <svg class="icon" width="101" height="44" viewBox="0 0 101 44" fill="#000000">
-      <use xlink:href="img/sprite.svg#logo-small"></use>
-    </svg>
-  </button>
-  ${state}
-</header>`;
+            <button class="back">
+              <span class="visually-hidden">Вернуться к началу</span>
+              <svg class="icon" width="45" height="45" viewBox="0 0 45 45" fill="#000000">
+                <use xlink:href="img/sprite.svg#arrow-left"></use>
+              </svg>
+              <svg class="icon" width="101" height="44" viewBox="0 0 101 44" fill="#000000">
+                <use xlink:href="img/sprite.svg#logo-small"></use>
+              </svg>
+            </button>
+            ${stateTemplate}
+          </header>`;
 };
 
 export default getHeaderTemplate;
--- js/templates/state.js
+++ js/templates/state.js
@@ -1,13 +1,15 @@
 import INITIAL_GAME_STATE from '../data';
 
-const getStateTemplate = (lives, timeLimit) => `<div class="game__timer">${timeLimit}</div>
-<div class="game__lives">
-  ${new Array(INITIAL_GAME_STATE.lives - lives)
-    .fill(`<img src="img/heart__empty.svg" class="game__heart" alt="Missed Life" width="31" height="27">`)
-    .join(``)}
-  ${new Array(lives)
-    .fill(`<img src="img/heart__full.svg" class="game__heart" alt="Life" width="31" height="27">`)
-    .join(``)}
-</div>`;
+const getStateTemplate = (lives, timeLimit) => {
+  return `<div class="game__timer">${timeLimit}</div>
+          <div class="game__lives">
+            ${new Array(INITIAL_GAME_STATE.lives - lives)
+              .fill(`<img src="img/heart__empty.svg" class="game__heart" alt="Missed Life" width="31" height="27">`)
+              .join(``)}
+            ${new Array(lives)
+              .fill(`<img src="img/heart__full.svg" class="game__heart" alt="Life" width="31" height="27">`)
+              .join(``)}
+          </div>`;
+};
 
 export default getStateTemplate;
