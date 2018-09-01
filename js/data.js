1039a656ed131165a62e3a1a47542c703481ed72
alexvlife <alexvlife@gmail.com>
2018-09-01 16:44:23 +0300

    m4. t2. described the data structure (INITIAL_GAME_STATE, and questions mock)

---
+++ js/data.js
@@ -0,0 +1,6 @@
+const INITIAL_GAME_STATE = Object.freeze({
+  lives: 3,
+  level: 1,
+});
+
+export default INITIAL_GAME_STATE;
---
+++ js/mocks/questions.js
@@ -0,0 +1,43 @@
+const questions = [
+  {
+    description: `Угадай, фото или рисунок?`,
+    images: [
+      {
+        url: `https://k42.kn3.net/CF42609C8.jpg`,
+        type: `paint`,
+      }
+    ]
+  },
+  {
+    description: `Угадайте для каждого изображения фото или рисунок?`,
+    images: [
+      {
+        url: `https://k42.kn3.net/D2F0370D6.jpg`,
+        type: `paint`,
+      },
+      {
+        url: `https://i.imgur.com/DiHM5Zb.jpg`,
+        type: `photo`,
+      },
+    ]
+  },
+  {
+    description: `Найдите рисунок среди изображений`,
+    images: [
+      {
+        url: `https://k32.kn3.net/5C7060EC5.jpg`,
+        type: `paint`,
+      },
+      {
+        url: `http://i.imgur.com/1KegWPz.jpg`,
+        type: `photo`,
+      },
+      {
+        url: `http://i.imgur.com/DKR1HtB.jpg`,
+        type: `photo`,
+      },
+    ]
+  },
+];
+
+export default questions;
