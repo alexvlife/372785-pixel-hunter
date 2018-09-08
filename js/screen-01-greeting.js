c3820370bab05216accbce530f1a7eaf524933c8
alexvlife <alexvlife@gmail.com>
2018-09-08 19:09:26 +0300

    m5. t2. added class ScreenGreetingView

--- js/screen-01-greeting.js
+++ js/screen-01-greeting.js
@@ -1,33 +1,10 @@
-import {render, showScreen} from './utilsForBrowser';
+import ScreenGreetingView from './view/screen-greeting-view';
+import {showScreen} from './utilsForBrowser';
 import rules from './screen-02-rules';
 
-const greetingScreenTemplate = `<section class="greeting central--blur">
-                          <img class="greeting__logo" src="img/logo_ph-big.svg" width="201" height="89" alt="Pixel Hunter">
-                          <div class="greeting__asterisk asterisk"><span class="visually-hidden">Я просто красивая звёздочка</span>*</div>
-                          <div class="greeting__challenge">
-                          <h3 class="greeting__challenge-title">Лучшие художники-фотореалисты бросают тебе вызов!</h3>
-                          <p class="greeting__challenge-text">Правила игры просты:</p>
-                          <ul class="greeting__challenge-list">
-                            <li>Нужно отличить рисунок от фотографии и сделать выбор.</li>
-                            <li>Задача кажется тривиальной, но не думай, что все так просто.</li>
-                            <li>Фотореализм обманчив и коварен.</li>
-                            <li>Помни, главное — смотреть очень внимательно.</li>
-                          </ul>
-                          </div>
-                          <button class="greeting__continue" type="button">
-                          <span class="visually-hidden">Продолжить</span>
-                          <svg class="icon" width="64" height="64" viewBox="0 0 64 64" fill="#000000">
-                            <use xlink:href="img/sprite.svg#arrow-right"></use>
-                          </svg>
-                          </button>
-                        </section>`;
+const screenGreetingView = new ScreenGreetingView();
+screenGreetingView.onClickButton = () => showScreen(rules);
 
-const greetingScreenElement = render(greetingScreenTemplate);
+const screenGreetingElement = screenGreetingView.element;
 
-const goNextButton = greetingScreenElement.querySelector(`.greeting__continue`);
-
-goNextButton.addEventListener(`click`, () => {
-  showScreen(rules);
-});
-
-export default greetingScreenElement;
+export default screenGreetingElement;
