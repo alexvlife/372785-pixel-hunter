e641aa4402e6799d845ed59fae29085beb7c09ea
alexvlife <alexvlife@gmail.com>
2018-09-14 16:13:35 +0300

    m7. t1. load questions data from server (added modal window with error)

--- js/router.js
+++ js/router.js
@@ -1,15 +1,33 @@
+import ScreenErrorPresenter from "./presenters/screen-error-presenter";
 import ScreenIntroPresenter from "./presenters/screen-intro-presenter";
 import ScreenGreetingPresenter from "./presenters/screen-greeting-presenter";
 import ScreenRulesPresenter from "./presenters/screen-rules-presenter";
 import ScreenGamePresenter from "./presenters/screen-game-presenter";
 import ScreenStatsPresenter from "./presenters/screen-stats-presenter";
 import GameModel from "./game-model";
-import questions from "./mocks/questions";
+
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+};
+
+let questionsData;
 
 export default class Router {
   static showScreenIntro() {
     const screenIntro = new ScreenIntroPresenter();
     screenIntro.show();
+    window.fetch(`https://es.dump.academy/pixel-hunter/questions`).
+      then(checkStatus).
+      then((response) => response.json()).
+      then((data) => {
+        questionsData = data;
+      }).
+      then(() => Router.showScreenGreeting()).
+      catch(Router.showScreenError);
   }
 
   static showScreenGreeting() {
@@ -23,7 +41,7 @@ export default class Router {
   }
 
   static showScreenGame(playerName) {
-    const gameModel = new GameModel(playerName, questions);
+    const gameModel = new GameModel(playerName, questionsData);
     const game = new ScreenGamePresenter(gameModel);
     game.init();
   }
@@ -31,5 +49,10 @@ export default class Router {
   static showScreenStats(finalGameState) {
     const screenStatsLevel = new ScreenStatsPresenter(finalGameState);
     screenStatsLevel.show();
+  }
+
+  static showScreenError(error) {
+    const screenError = new ScreenErrorPresenter(error);
+    screenError.show();
   }
 }
---
+++ js/view/screen-error-view.js
@@ -0,0 +1,17 @@
+import AbstractView from "./abstract-view";
+
+export default class ScreenErrorView extends AbstractView {
+  constructor(error) {
+    super();
+    this.error = error;
+  }
+
+  get template() {
+    return `<section class="modal">
+              <div class="modal__inner">
+                <h2 class="modal__title">Произошла ошибка!</h2>
+                <p class="modal__text modal__text--error">${this.error.name} ${this.error.message}</p>
+              </div>
+            </section>`;
+  }
+}
